5e84f01e5f961871156f1896fba9ffb451c8aa26
ProfessorZed <q812121212@hotmail.com>
2016-09-12 13:39:07 +0100

    fixes

--- src/me/zed/ranks/chestClicks.java
+++ src/me/zed/ranks/chestClicks.java
@@ -9,6 +9,7 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.material.MaterialData;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,23 +25,27 @@ public class chestClicks implements Listener {
 
     @EventHandler
     public void chest(InventoryClickEvent e) {
-        e.setCancelled(true);
         Player p = (Player)e.getWhoClicked();
-        if(e.getCurrentItem().getType() == Material.WOOL){
+        if(e.getCurrentItem().getType() == Material.WOOL && e.getInventory() != p.getInventory()){
+            e.setCancelled(true);
             colorsInv(p, p.getInventory());
         }
     }
 
     public void colorsInv(Player player, Inventory inventory){
         Inventory inv = Bukkit.createInventory(player, 9*2, "Rank Colors");
-
+        addWool(inv);
         player.openInventory(inv);
     }
 
     public void addWool(Inventory inventory){
+        lores.add("§e§oClick to Activate!");
         for(int i = 1; i <= 15; ++i){
             ItemStack wool = new ItemStack(Material.WOOL, 1, (byte)i);
-            ItemMeta woolmeta = wool.getItemMeta();
+            ItemMeta meta = wool.getItemMeta();
+            meta.setLore(lores);
+            wool.setItemMeta(meta);
+            inventory.addItem(wool);
         }
     }
 }
